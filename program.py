86c220d3faea427fc73902a0a8f9e118ae26aaab
student <nknnjnknn@ere>
2023-11-23 17:42:20 +0300

    supperfunction added

--- program.py
+++ program.py
@@ -1,6 +1,11 @@
+def my_superfunction():
+    print('What an awesome print!')
+
+
 def main():
     print('My first git program')
     print('and i change it everyday')
+    my_superfunction()
 
 
 if __name__ == '__main__':
